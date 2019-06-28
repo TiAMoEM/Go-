667ea3f303b57c2db1cc0ad80549cedf5e7c62ac
yangjiayi <415878598@qq.com>
2019-06-28 10:58:19 +0800

    方法应用

--- src/go_code/chapter13/demo04/main.go
+++ src/go_code/chapter13/demo04/main.go
@@ -14,8 +14,36 @@ func (p Person) test() {
 	fmt.Println("test() name=", p.Name)
 }
 
+func (p Person) speak() {
+	fmt.Println(p.Name, "is a good man")
+}
+
+func (p Person) calculate() {
+	res := 0
+	for i := 1; i <= 100; i++ {
+		res += i
+	}
+	fmt.Println(p.Name, "result =", res)
+}
+
+func (p Person) calculate2(n int) {
+	res := 0
+	for i := 1; i <= n; i++ {
+		res += i
+	}
+	fmt.Println(p.Name, "result2 =", res)
+}
+
+func (p Person) getSum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
 func main() {
 	var p Person
 	p.Name = "Bill"
 	p.test()
+	p.speak()
+	p.calculate()
+	p.calculate2(20)
+	fmt.Println(p.getSum(10, 20))
 }
