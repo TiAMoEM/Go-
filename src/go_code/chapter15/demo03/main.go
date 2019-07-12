3d8e96ad3cf90eb99bf4edb48462360facadede5
yangjiayi <415878598@qq.com>
2019-07-12 14:48:14 +0800

    写文件

---
+++ src/go_code/chapter15/demo03/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filePath := "E:\\test\\abc.txt"
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("open file err = %v", err)
+		return
+	}
+	defer file.Close()
+	str := "Hello World\n"
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 5; i++ {
+		writer.WriteString(str)
+	}
+	writer.Flush()
+}
