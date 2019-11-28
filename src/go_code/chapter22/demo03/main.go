f40949f003784b6ee5cbda6e5b4bba6e60cd6d7e
yangjiayi <415878598@qq.com>
2019-11-28 17:43:58 +0800

    练习

--- src/go_code/chapter22/demo03/main.go
+++ src/go_code/chapter22/demo03/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "unicode/utf8"
 
 func FizzBuzz() {
 	i := 1
@@ -18,10 +19,33 @@ func FizzBuzz() {
 	}
 }
 
+func ReverseString(str string) string {
+	s := []rune(str)
+	// for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+	// 	s[i], s[j] = s[j], s[i]
+	// }
+	// return string(s)
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
+	}
+	return string(s)
+
+}
+
 func main() {
 	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for i := 0; i < len(arr); i++ {
 		fmt.Printf("arr[%d] = %d\n", i, arr[i])
 	}
 	FizzBuzz()
+
+	str := "dsjkaisdyfhaisdhf...i a suhfi"
+	fmt.Printf("String %s\n Length: %d, Runes: %d\n", str, len([]byte(str)), utf8.RuneCount([]byte(str)))
+	s := str
+	r := []rune(s)
+	copy(r[4:4+3], []rune("abc"))
+	fmt.Printf("Before: %s\n", s)
+	fmt.Printf("After: %s\n", string(r))
+	str1 := "foobar"
+	fmt.Println(ReverseString(str1))
 }
