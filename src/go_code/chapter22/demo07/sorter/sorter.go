3bda5ab6629d4ed8c35f308256be2cccf2ed29bb
yangjiayi <415878598@qq.com>
2019-12-18 16:26:04 +0800

    一个简单的程序例子

---
+++ src/go_code/chapter22/demo07/sorter/sorter.go
@@ -0,0 +1,85 @@
+package main
+
+import "flag"
+import "fmt"
+import "io"
+import "bufio"
+import "os"
+import "strconv"
+import "time"
+import "algorithms/bubblesort"
+import "algorithms/qsort"
+
+var infile *string = flag.String("i", "infile", "File contains values for sorting")
+var outfile *string = flag.String("o", "outfile", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
+
+func reaValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file", infile)
+		return
+	}
+	defer file.Close()
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			return
+		}
+		str := string(line)
+		value, err2 := strconv.Atoi(str)
+		if err2 != nil {
+			err = err2
+			return
+		}
+		values = append(values, value)
+	}
+	return
+}
+
+func writeValues(values []int, outfile string) error {
+	file, err := os.Create(outfile)
+	if err != nil {
+		fmt.Println("Failed to create the output file", outfile)
+		return err
+	}
+	defer file.Close()
+	for _, value := range values {
+		str := strconv.Itoa(value)
+		file.WriteString(str + "\n")
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if infile != nil {
+		fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+	}
+	values, err := reaValues(*infile)
+	if err != nil {
+		t1 := time.Now()
+		switch *algorithm {
+		case "qsort":
+			qsort.QuickSort(values)
+		case "bubblesort":
+			bubblesort.BubbleSort(values)
+		default:
+			fmt.Println("Sorting algorithm", *algorithm, "is either unknown or unsuported")
+		}
+		t2 := time.Now()
+		fmt.Println("The sorting process costs", t2.Sub(t1), "to complete")
+		writeValues(values, *outfile)
+	} else {
+		fmt.Println(err)
+	}
+}
